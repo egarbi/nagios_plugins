b9f09e380b2c84b8c69372ea32e31bf0176f8735
Enrique Garbi <enrique.garbi@cloudreach.com>
2018-09-05 13:39:53 +0100

    Size check: supports thresholds and list of files

--- s3/check_s3_file_size.py
+++ s3/check_s3_file_size.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
 
-import datetime
-from datetime import timedelta, tzinfo
+##########################################################
+#
+# Written by Matthew McMillan
+# matthew.mcmillan@gmail.com
+# @matthewmcmillan
+# https://matthewcmcmillan.blogspot.com
+# https://github.com/matt448/nagios-checks
+#
+# Modified by quique@enriquegarbi.com.ar
+# https://github.com/egarbi/nagios_plugins/tree/master/s3
+#
+# -- Nagios error codes --
+#    0 = OK/green
+#    1 = WARNING/yellow
+#    2 = CRITICAL/red
+#    3 = UNKNOWN/purple
+
 import botocore
 import boto3
 import argparse
-import re
-from botocore.config import Config
 
 # Parse command line arguments
 parser = argparse.ArgumentParser(description='This script is a Nagios check that \
-                                              monitors the age of files that have \
-                                              been backed up to an S3 bucket.')
+                                              monitors the size of a list of files in \
+                                              an S3 bucket.')
 
 parser.add_argument('--profile', dest='profile', type=str, required=False, default='default',
                     help='AWS profile to use to connect')
@@ -19,11 +32,20 @@ parser.add_argument('--profile', dest='profile', type=str, required=False, defau
 parser.add_argument('--bucketname', dest='bucketname', type=str, required=True,
                     help='Name of S3 bucket')
 
-parser.add_argument('--bucketfolder', dest='bucketfolder', type=str, default='',
-                    help='Folder to check inside bucket (optional).')
-
 parser.add_argument('--proxyhost', dest='proxyhost', type=str, default='',
                     help='Proxy to establish a connection trough (optional).')
+
+parser.add_argument('--warning', dest='warning', type=int, default=0,
+                    help='The Warning threshold in bytes for size \
+                          Default is 0 (It will never fail) \
+                          ')
+
+parser.add_argument('--critical', dest='critical', type=int, default=0,
+                    help='The Critical threshold in bytes for size \
+                          Default is 0 (It will never fail).')
+
+parser.add_argument('--files', dest='files', type=str, required=True,
+                    help='List of files to be checked within the bucketname (ie: ["path/to/one/file","path/to/another/file"])')
 
 parser.add_argument('--debug', action='store_true',
                     help='Enables debug output.')
@@ -32,16 +54,16 @@ args = parser.parse_args()
 
 # Assign variables from command line arguments
 bucketname = args.bucketname
-bucketfolder = args.bucketfolder
-bucketfolder_regex = '^' + bucketfolder
 proxyhost = args.proxyhost
-
-filecount = 0
+warning = args.warning
+critical = args.critical
+files = args.files.split(",")
+criticallist = []
+warninglist = []
 
 if (args.debug):
     print '########## START DEBUG OUTPUT ############'
     print 'DEBUG: S3 BUCKET NAME: ' + str(bucketname)
-
 
 if (args.debug):
     print "DEBUG: Connecting to S3"
@@ -78,23 +100,35 @@ else:
 if (args.debug):
     print "Bucket: %s" % bucket
 
-# Loop through keys (files) in the S3 bucket and
-# check each one for min and max file age.
-for key in bucket.objects.filter(Prefix=bucketfolder):
-        if key.size == 0:
-            filecount += 1
+if (args.debug):
+    print 'WARNING Size Thresholds: {0} bytes'.format(warning)
 
-msg = 'There is/are %s empty file/s in this bucket: ' % str(filecount)
-if filecount > 0:
+if (args.debug):
+    print 'CRITICAL Size Thresholds: {0} bytes'.format(critical)
 
-    statusline = 'CRITICAL: ' + msg
+for file in files:
+    object = s3.Object(bucketname,file)
+    size = object.content_length
+    if (args.debug):
+      print ' Size of {0} is {1} bytes'.format(file,size)
+    if size < critical:
+        criticallist.append(file)
+    elif size > critical and size < warning:
+        warninglist.append(file)
+
+lengthCritical = len(criticallist)
+lengthWarning = len(warninglist)
+if lengthCritical > 0 and lengthWarning > 0:
+    print 'CRITICAL: There are {0} files in critical status: {1} and {2} in warning status: {3} '.format(lengthCritical, criticallist, lengthWarning, warninglist)
     exitcode = 2
-elif filecount == 0:
-    statusline = 'OK: ' + msg
-    exitcode = 0
+elif lengthCritical > 0 and lengthWarning == 0:
+    print 'CRITICAL: There are {0} files in critical status: {1}'.format(lengthCritical, criticallist)
+    exitcode = 2
+elif lengthCritical == 0 and lengthWarning > 0:
+    print 'WARNING: There are {0} files: in warning status: {1}'.format(lengthWarning, warninglist)
+    exitcode = 1
 else:
-    statusline = 'UNKNOWN: ' + msg
-    exitcode = 3
+    print 'OK: All files are within the size boundaries'
+    exitcode = 0
 
-print statusline
 exit(exitcode)
